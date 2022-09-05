f4022787fa1bdb007d839dd7d82a0880808fd694
EFuriati <77512731+EFuriati@users.noreply.github.com>
2022-09-05 15:16:52 -0300

    Add files via upload

--- esp32vgacalendar.ino
+++ esp32vgacalendar.ino
@@ -1,4 +1,21 @@
+/* esp32vgacalendar - Calendar with VGA output for esp32
+    Copyright (C) 2022 Eduardo Furiati
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+    
 #include <WiFi.h>
 #include "fabgl.h" // http://www.fabglib.org/
 #include <ESPAsyncWebServer.h>
